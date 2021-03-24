6aabed3361835b2049c2c2ee29b722762f76b689
Dreycey <albindreycey@gmail.com>
2021-03-24 15:23:34 -0600

    creating initial pipeline for the codebase

---
+++ src/smellml.py
@@ -0,0 +1,18 @@
+#! usr/bin/python3
+
+"""
+
+"""
+
+class CodeBase:
+    
+    def __init__(self, path):
+        """ constructor for the CodeBase Class """
+        self.codebase_path = path
+
+def main():
+    """ This controls how the script for the smellML works"""
+    
+
+if __name__ == "__main__":
+    main()
