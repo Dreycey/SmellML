b1bfa238dc803e52a5727a8daffd2e1eaf631ba1
latifa <leotifa@gmail.com>
2021-04-23 16:10:19 -0600

    Updated domain extraction script

--- smellml_modules/extract_domains_old.py
+++ smellml_modules/extract_domains_old.py
@@ -1,18 +1,13 @@
 #! /usr/bin/env python3
 """
 Extracts topics (domains/tags) from given github repo
-
-USAGE:
-    python extract_domains.py <CSV_file>
-    python extract_domains.py <"owner/project">
 """
 import sys
 import csv
 import time
 import github
-from progressbar import progressbar
-from tqdm import tqdm
 from github import Github
+import requests
 
 """
 Extracts topics (domains/tags) from given github repo and returns
@@ -32,7 +27,7 @@ def get_topics(git_obj, git_project):
         # retrieve topics form repo object and store in dictionary
         topics.append(repo.get_topics())
     except github.UnknownObjectException as e:
-        print(f'Project name: {git_project}, Error: {e}')
+        print(e)
 
     return topics
 
@@ -40,53 +35,45 @@ def get_topics(git_obj, git_project):
 def get_topics_from_csv(git_obj, csv_file):
     # "Project Name","URL","ML libraries","Number of Contributors","Number of Stars"
     topics_dict = {}
-    url_dict = {}
-    MAX_READS = 100
-
     with open(csv_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        # skip header
-        next(csv_reader, None)
         line_count = 0
-        for row in tqdm(csv_reader, total=MAX_READS):
-            # limit reads to 100 due to github api query restrictions
-            if line_count > MAX_READS:
-                return topics_dict, url_dict
-            else:
-                topics_dict[row[0]] = get_topics(git_obj, row[0])
-                url_dict[row[0]] = row[1]
-                line_count += 1
-        #print(f'Processed {line_count} lines.')
-
-    return topics_dict, url_dict
+        for row in csv_reader:
+            next(csv_reader)
+            #print(get_topics(git_obj, row[0]))
+            topics_dict[row[0]] = get_topics(git_obj, row[0])
+            line_count += 1
+        print(f'Processed {line_count} lines.')
+    return topics_dict
 
 
-def save_to_csv(dict1, dict2):
+def save_to_csv(dict):
     with open("project_topics.csv", mode='w') as csv_file:
-        fieldnames = ['project_name', 'url', 'topics']
+        fieldnames = ['project_name', 'topics']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
 
-        for key in dict1:
-            writer.writerow({'project_name': key, 'url': dict2[key], 'topics': dict1[key]})
-    csv_file.close()
+        for key in dict:
+            writer.writerow({'project_name': key, 'topics': data_dict[key]})
+
 
 def main():
-    # using an access token allows for more queries
-    # gitObj = Github("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")
-    # create github object without an access token (limits queries per hour)
+    """ controls the script """
     gitObj = Github()
-
+    # # using an access token
+    # gitObj = Github("access_token")
+    #
+    # # Github Enterprise with custom hostname
+    # gitObj = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
     topics = {}
-    urls = {}
 
     ### SCRIPT INPUT
     input = sys.argv[1]
 
     # TEST
     if '.csv' in input:
-        (topics,urls) = get_topics_from_csv(gitObj, input)
-        save_to_csv(topics, urls)
+        topics = get_topics_from_csv(gitObj, input)
+        save_to_csv(topics)
     else:
         print(f'Project: {input}, Topics: {get_topics(gitObj, input)}')
 
